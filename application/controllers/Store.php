fab411d58c5c0fb73ab11fb274e8374f5cb5eff3
ecoknows <jerico.villaraza19@gmail.com>
2022-09-06 09:36:41 +0800

    :bug: remove service in get all stores

--- application/controllers/Store.php
+++ application/controllers/Store.php
@@ -23,7 +23,7 @@ class Store extends CI_Controller {
 				if($address){
 					$stores = $this->store_model->get_stores_available($this->google->geolocator($address)['lat'],$this->google->geolocator($address)['lng'],$service);
 				}else{
-					$stores = $this->store_model->get_stores_available($service);
+					$stores = $this->store_model->get_stores_available();
 				}
 
 				$response = array(
