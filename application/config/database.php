37680d81c087d78b341c86a23f61bfc759dd1146
ecoknows <eco.villaraza@gmail.com>
2022-10-20 13:59:59 +0800

    :construction: bsc backend abd database in progress

--- application/config/database.php
+++ application/config/database.php
@@ -28,3 +28,26 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+
+$db['bsc'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => $_ENV['BSC_DATABASE_USERNAME'],
+	'password' => $_ENV['BSC_DATABASE_PASSWORD'],
+	'database' => $_ENV['BSC_DATABASE_NAME'],
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
