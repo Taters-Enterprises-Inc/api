e11d8af5d6ca4cd7b3ddbbdead5d0ea0af32554c
ecoknows <jerico.villaraza19@gmail.com>
2022-11-04 18:38:45 +0800

    :bug: add payment controller

---
+++ application/controllers/Payment.php
@@ -0,0 +1,18 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
+date_default_timezone_set('Asia/Manila');
+
+
+class Payment extends CI_Controller
+{
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+}
