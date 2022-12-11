2fc2078eb416130421a71b9df2a8698030b0dc2d
ecoknows <jerico.villaraza19@gmail.com>
2022-12-12 01:07:32 +0800

    :bug: remove load_image_product

--- application/controllers/Image.php
+++ application/controllers/Image.php
@@ -35,34 +35,6 @@ class Image extends CI_Controller {
 		echo $res;
 	}
 
-	public function load_image_product($file_name)
-	{	
-		$file_extension = strtolower(substr(strrchr($file_name,"."),1));
-
-		switch( $file_extension ) {
-			case "gif": $ctype="image/gif"; break;
-			case "png": $ctype="image/png"; break;
-			case "jpeg":
-			case "jpg": $ctype="image/jpeg"; break;
-			case "svg": $ctype="image/svg+xml"; break;
-			default:
-		}
-
-		header('Content-type: ' . $ctype);
-		// $url = img_url().$file_name;
-		$url = img_url() . 'shared/products/75/'.$file_name;
-	
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		$res = curl_exec($ch);
-		$rescode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
-		curl_close($ch) ;
-		echo $res;
-	}
-
 	public function load_image_user_discount($file_name)
 	{	
 		$file_extension = strtolower(substr(strrchr($file_name,"."),1));
