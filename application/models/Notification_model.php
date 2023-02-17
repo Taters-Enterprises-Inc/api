c4708d003cd3ee36476888d4bfd043e301262d09
ecoknows <jerico.villaraza19@gmail.com>
2023-02-18 02:35:24 +0800

    :ambulance: hotfix deals redeems tb id in notification

--- application/models/Notification_model.php
+++ application/models/Notification_model.php
@@ -48,9 +48,11 @@ class Notification_model extends CI_Model {
             
             C.id as transaction_tb_id,
             D.id as catering_transaction_tb_id,
+            E.id as deals_redeems_tb_id,
             
             C.tracking_no,
             D.tracking_no as catering_tracking_no,
+            E.redeem_code as redeem_code,
             
         ');
 
@@ -59,6 +61,7 @@ class Notification_model extends CI_Model {
         
         $this->db->join('transaction_tb C', 'C.id = B.transaction_tb_id','left');
         $this->db->join('catering_transaction_tb D', 'D.id = B.catering_transaction_tb_id','left');
+        $this->db->join('deals_redeems_tb E', 'E.id = B.deals_redeems_tb_id','left');
 
         if($is_unseen){
             $this->db->where('A.dateseen', NULL);
