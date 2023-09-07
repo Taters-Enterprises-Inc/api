d3cd269310e6e65ff1f52abba3d54c6614cd2075
knhrvs <knhrvs.dev@gmail.com>
2023-09-07 11:13:00 +0800

    🔧 Minor adjustment

--- application/models/Stock_ordering_model.php
+++ application/models/Stock_ordering_model.php
@@ -697,7 +697,7 @@ class Stock_ordering_model extends CI_Model {
     
     public function filename_factory_prefix($order_id, $si_type){
 
-        $this->db->select('c.company_code, B.category_name')
+        $this->db->select('C.company_code, B.category_name')
                  ->from('order_information_tb A')
                  ->join('category_tb B', 'B.category_id = A.order_type_id', 'left')
                  ->join($this->newteishop->database.'.store_tb C', 'C.store_id = A.store_id', 'left')
