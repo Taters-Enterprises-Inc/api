d846c1d2417096d101ca2e27b55b2c52f7f18ab2
Eco Villaraza <eco.villaraza19@gmail.com>
2023-10-13 07:02:33 +0800

    :recycle: array product to single product, remove loop.

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -221,55 +221,54 @@ class Cart extends CI_Controller {
                 
 				$_POST = json_decode(file_get_contents("php://input"), true);
 
-                $products = $this->input->post('products');
+                $product = $this->input->post('product');
 
                 $items = array();
-                foreach($products as $product){
-                    $prod_id = $product['prod_id'];
-                    $product_details = $this->shop_model->get_details($prod_id)[0];
-                    $prod_image_name = $product['prod_image_name'];
-    
-                    $product_sku_price = 0;
-                    $prod_size = NULL;
-                    
-                    $varx = array();
-                    
-                    if($product['prod_size']){
-                        
-                        $varx[] = $product['prod_size'];
-                        $prod_size = $this->shop_model->fetch_variants_details($product['prod_size']);
 
-                        $varxz = array_values(array_filter($varx));
-                        $product_sku = $this->shop_model->fetch_product_sku($varxz);
-                        if(!empty($product_sku)){
-                            $product_sku_price = $product_sku->price;
-                        }else{
-                            $product_sku_price = $product_details->price;
-                        }
+                $prod_id = $product['prod_id'];
+                $product_details = $this->shop_model->get_details($prod_id)[0];
+                $prod_image_name = $product['prod_image_name'];
+
+                $product_sku_price = 0;
+                $prod_size = NULL;
+                
+                $varx = array();
+                
+                if($product['prod_size']){
+                    
+                    $varx[] = $product['prod_size'];
+                    $prod_size = $this->shop_model->fetch_variants_details($product['prod_size']);
+
+                    $varxz = array_values(array_filter($varx));
+                    $product_sku = $this->shop_model->fetch_product_sku($varxz);
+                    if(!empty($product_sku)){
+                        $product_sku_price = $product_sku->price;
+                    }else{
+                        $product_sku_price = $product_details->price;
                     }
-
-                    $product_price = (empty($varx)) ? $product_details->price : $product_sku_price;
-                    $prod_calc_amount   = $product['prod_calc_amount'];
-    
-                    $set_value['prod_id']               = $prod_id;
-                    $set_value['prod_image_name']       = $prod_image_name;
-                    $set_value['prod_name']             = $product_details->name;
-                    $set_value['prod_qty']              = $product['prod_qty'];
-                    $set_value['prod_price']            = $product_price;
-                    $set_value['prod_calc_amount']      = $prod_calc_amount;
-                    $set_value['prod_with_drinks']      = isset($product['prod_with_drinks']) ? 1 : 0;
-                    $set_value['prod_size']             = (empty($prod_size)) ? '' : $prod_size->name;
-                    $set_value['prod_size_id']          = $product['prod_size'] ?? '';
-                    $set_value['prod_multiflavors']     = $product['flavors_details'] ?? '';
-                    $set_value['prod_sku_id']           = $product['prod_sku_id'] ?? '';
-                    $set_value['prod_sku']              = $product['prod_sku'] ?? '';
-                    $set_value['prod_discount']         = 0;
-                    $set_value['prod_category']         = $product_details->category;
-                    $set_value['prod_type']             = $product['prod_type'];
-                    $set_value['promo_discount_percentage'] = $product['promo_discount_percentage'] ?? '';
-    
-                    $items[] = $set_value;
                 }
+
+                $product_price = (empty($varx)) ? $product_details->price : $product_sku_price;
+                $prod_calc_amount   = $product['prod_calc_amount'];
+
+                $set_value['prod_id']               = $prod_id;
+                $set_value['prod_image_name']       = $prod_image_name;
+                $set_value['prod_name']             = $product_details->name;
+                $set_value['prod_qty']              = $product['prod_qty'];
+                $set_value['prod_price']            = $product_price;
+                $set_value['prod_calc_amount']      = $prod_calc_amount;
+                $set_value['prod_with_drinks']      = isset($product['prod_with_drinks']) ? 1 : 0;
+                $set_value['prod_size']             = (empty($prod_size)) ? '' : $prod_size->name;
+                $set_value['prod_size_id']          = $product['prod_size'] ?? '';
+                $set_value['prod_multiflavors']     = $product['flavors_details'] ?? '';
+                $set_value['prod_sku_id']           = $product['prod_sku_id'] ?? '';
+                $set_value['prod_sku']              = $product['prod_sku'] ?? '';
+                $set_value['prod_discount']         = 0;
+                $set_value['prod_category']         = $product_details->category;
+                $set_value['prod_type']             = $product['prod_type'];
+                $set_value['promo_discount_percentage'] = $product['promo_discount_percentage'] ?? '';
+
+                $items[] = $set_value;
 
                 if($this->session->orders){
                     $_SESSION['orders'] = array_merge($this->session->orders,$items);
