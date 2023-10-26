d4f2d66049b5e060c849711ce4f750a18918c046
Michael Aquino <aquinomike96@gmail.com>
2023-10-26 20:23:19 +0800

    Added DB configuration for HR Appraisal project

--- application/config/database.php
+++ application/config/database.php
@@ -97,4 +97,26 @@ $db['stock-ordering'] = array(
 	'save_queries' => TRUE
 );
 
+$db['hr-appraisal'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => $_ENV['DATABASE_USERNAME'],
+	'password' => $_ENV['DATABASE_PASSWORD'],
+	'database' => $_ENV['HR_APPRAISAL_DATABASE_NAME'],
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
 
